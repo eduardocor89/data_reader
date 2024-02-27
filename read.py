436f7b234f361201ac17a30e66938ae452337306
Eddie Cordero <78165600+eduardocor89@users.noreply.github.com>
2024-02-27 08:56:20 -0500

    Add files via upload

--- read.py
+++ read.py
@@ -1,10 +1,11 @@
 import csv
 import csv
-
+from collections import Counter
 
 with open('read.csv', 'r') as file: 
     csv_reader = csv.DictReader(file)
     data = []
+    same_property_visit = {}
     # read each line and pull from it the property name and date
     # of your visit
     for row in csv_reader:
@@ -16,11 +17,20 @@ with open('read.csv', 'r') as file:
         #print(visit)
 
         # Read each row and find the word 'follow' 
-        if 'follow' in data[item]['Comments']:
-            print("You should follow up on: " + property_name)
-            print("Your last visit was on: " + date)
+        #if 'follow' in data[item]['Comments']:
+            #print("You should follow up on: " + property_name)
+            #print("Your last visit was on: " + date)
 
-    # I want to count
+        # I want to count the number of times I visited
+        # the same property
+        counter = 1
+        if property_name not in same_property_visit:
+            same_property_visit[property_name] = counter
+        else:
+            same_property_visit[property_name] += counter
+        # print the list in order of most visited to least
+        print(Counter(same_property_visit))
+
   
     # date = data[0]['Date'] 
     # name = data[0]['Property Name']
